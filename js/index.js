a9e4ce10170b5d4169222883da077601b4c3b962
Hanna Syniavska <62648675+HannaSyn@users.noreply.github.com>
2021-01-12 17:42:26 +0200

    Create landing page for company

---
+++ js/index.js
@@ -0,0 +1,31 @@
+const animItems = document.querySelectorAll('.anim-items');
+
+function animateOnScroll() {
+  for( let i = 0; i < animItems.length; i++ ) {
+    const animItem = animItems[i];
+    const animItemHeigh = animItem.offsetHeight;
+    const animItemOffset = offset(animItem).top;
+    const animStart = 4;
+
+    let animItemPoint = window.innerHeight - animItemHeigh / animStart;
+
+    if( animItemHeigh >  window.innerHeight) {
+      animItemPoint = window.innerHeight - window.innerHeight / animStart;
+    }
+
+    if((pageYOffset > (animItemOffset - animItemPoint)) && (pageYOffset < (animItemOffset + animItemHeigh))) {
+      animItem.classList.add('active');
+    } else {
+      animItem.classList.remove('active');
+    }
+  }
+}
+
+function offset(el) {
+  const rect = el.getBoundingClientRect(),
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+    scrollTop = window.pageXOffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+}
+
+window.addEventListener('scroll', animateOnScroll);
